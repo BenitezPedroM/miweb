864987a7af7db7c48b1437a56f22d9e38d8ec5c1
Benitez Pedro <benitezpedromatias@gmail.com>
2026-04-09 12:51:22 -0300

    mi primer programa funcionando

---
+++ python-proyectos/mi_primer_programa.py
@@ -0,0 +1,8 @@
+# Mi primer programa en Python
+print("¡Hola Mundo!")
+
+nombre = input("¿como te llamas?")
+print(f"mucho gusto {nombre}")
+
+edad = input("¿cuantos años tenes?")
+print(f"En 10 años vas a tener {int(edad) + 10} años")
